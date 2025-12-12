a8a7667778b2b79ed4487509ea6b7d6b7eb9e992
betulozcelik <betul.ozceliik.2004@gmail.com>
2025-12-12 20:48:09 +0300

    Device dosyası ve CMake düzenlendi

--- include/Device.h
+++ include/Device.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <iostream>
+#include <string>
+
+class Device {
+protected:
+    static int idCounter; 
+    int id;
+    std::string name;
+    std::string status;
+    bool isActive;
+
+public:
+    Device(std::string n);
+    virtual ~Device();
+
+    int getId() const;
+    std::string getName() const;
+
+    virtual void setActive(bool state);
+    virtual void powerOn();
+    virtual void powerOff();
+
+    virtual void reportStatus() const;
+
+    virtual Device* clone() const = 0;
+};
+
+#endif
+
