82bda5d9a8993babf0aee2f874fa8e9eca8bb0b2
Pelin Keskin <220206006@ostimteknik.edu.tr>
2025-12-13 20:03:57 +0300

    Improve state restriction output in MSH

--- src/MSH.cpp
+++ src/MSH.cpp
@@ -39,18 +39,43 @@ void MSH::revertToPreviousState() {
 
 // --- restrictions/log (LLR 4.5) ---
 void MSH::applyRestrictions() const {
+    std::cout << "[MSH] DURUM RAPORU\n";
     switch (currentState) {
         case State::Normal:
-            std::cout << "[MSH] State=Normal" << std::endl;
+            std::cout << "[DURUM] Normal Mod\n";
+            std::cout << "[ACIKLAMA] Standart calisma: temel islevler aktif.\n";
+            std::cout << "[KISITLAR]\n";
+            std::cout << " - Yok (standart mod)\n";
+            std::cout << "[ACIK KALANLAR]\n";
+            std::cout << " - Tum kullanici islemleri\n";
+            std::cout << " - Cihazlar normal calisir\n";
             break;
         case State::HighPerformance:
-            std::cout << "[MSH] State=HighPerformance" << std::endl;
+            std::cout << "[DURUM] Yuksek Performans Modu\n";
+            std::cout << "[ACIKLAMA] Performans oncelikli: kaynak kullanimi artar.\n";
+            std::cout << "[KISITLAR]\n";
+            std::cout << " - Enerji tasarrufu azaltildi / devre disi\n";
+            std::cout << " - Arka plan optimizasyonlari kapatildi\n";
+            std::cout << "[ACIK KALANLAR]\n";
+            std::cout << " - Tum kullanici islemleri\n";
+            std::cout << " - Sistem maksimum performansta\n";
             break;
         case State::Sleep:
-            std::cout << "[MSH] State=Sleep (some operations disabled)" << std::endl;
+            std::cout << "[DURUM] Uyku (Sleep) Modu\n";
+            std::cout << "[ACIKLAMA] Enerji tasarrufu: kritik olmayan islevler kisitlanir.\n";
+
+            std::cout << "[KISITLAR]\n";
+            std::cout << " - Isiklar: KAPAT (simulasyon)\n";
+            std::cout << " - TV / Medya: KAPAT (simulasyon)\n";
+            std::cout << " - Agir islemler: DURDUR / ertele\n";
+            std::cout << " - Kullanici etkilesimi: SINIRLI (durum goruntule + uyandir)\n";
+
+            std::cout << "[ACIK KALANLAR]\n";
+            std::cout << " - Kritik servisler (ornek: sensorler)\n";
+            std::cout << " - Uyandir (Wake-up) islemleri\n";
             break;
         default:
-            std::cout << "[MSH] State=Unknown" << std::endl;
+            std::cout << "[DURUM] Bilinmeyen durum!\n";
             break;
     }
 }
